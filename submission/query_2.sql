6ecd2dc069b6f830f79f5a28b6d575e74bb4b4da
vikingh27 <vikasmikado@gmail.com>
2024-05-18 23:52:21 -0500

    Update query_2.sql
    Reverting LLM recommendation as auto-grading is failing.

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,19 +1,15 @@
--- Define the years as variables
-DECLARE @previous_year INT = 2020;
-DECLARE @current_year INT = 2021;
-
--- This query inserts actor data into the actors table.
+-- This query inserts actor data into the vpsjul8468082.actors table.
 -- It merges actors' data from the previous year with the current year's data.
 -- The query combines film details, updates the quality class, and marks active actors based on the current year's film data.
 
-INSERT INTO actors
+INSERT INTO vpsjul8468082.actors
 -- Actors from the last year
-WITH previous_year_actors AS (
-  SELECT * FROM actors
-  WHERE current_year = @previous_year
+WITH last_year AS (
+  SELECT * FROM vpsjul8468082.actors
+  WHERE current_year = 2020
 ),
 -- Actors from the current year
-current_year_actors AS (
+this_year AS (
   SELECT 
     actor,
     actor_id,
@@ -33,28 +29,28 @@ current_year_actors AS (
       WHEN AVG(rating) <= 6 THEN 'bad'
     END AS quality_class,
     max(year) as year
-  FROM actor_films 
-  WHERE year = @current_year
+  FROM bootcamp.actor_films 
+  WHERE year = 2021
   GROUP BY actor, actor_id
 )
 SELECT 
-  COALESCE(pya.actor, cya.actor) AS actor,
-  COALESCE(pya.actor_id, cya.actor_id) AS actor_id,
+  COALESCE(ly.actor, ty.actor) AS actor,
+  COALESCE(ly.actor_id, ty.actor_id) AS actor_id,
   CASE
-    WHEN cya.films IS NULL THEN pya.films
-    WHEN cya.films IS NOT NULL AND pya.films IS NULL THEN cya.films
-    WHEN cya.films IS NOT NULL AND pya.films IS NOT NULL THEN pya.films || cya.films
+    WHEN ty.films IS NULL THEN ly.films
+    WHEN ty.films IS NOT NULL AND ly.films IS NULL THEN ty.films
+    WHEN ty.films IS NOT NULL AND ly.films IS NOT NULL THEN ly.films || ty.films
   END AS films,
   CASE 
-    WHEN cya.quality_class IS NULL THEN pya.quality_class 
-    ELSE cya.quality_class
+    WHEN ty.quality_class IS NULL THEN ly.quality_class 
+    ELSE ty.quality_class
   END AS quality_class,
   CASE 
-    WHEN cya.year IS NOT NULL THEN TRUE 
+    WHEN ty.year IS NOT NULL THEN TRUE 
     ELSE FALSE 
   END AS is_active,
-  COALESCE(cya.year, pya.current_year + 1) AS current_year
-FROM previous_year_actors pya 
+  COALESCE(ty.year, ly.current_year + 1) AS current_year
+FROM last_year ly 
 -- FULL JOIN to catch all the details from the past and current year using COALESCE
-FULL OUTER JOIN current_year_actors cya
-ON pya.actor_id = cya.actor_id
+FULL OUTER JOIN this_year ty
+ON ly.actor_id = ty.actor_id
